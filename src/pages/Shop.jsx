2e68a6c0aa729f37a964eeae9e1d60a8ca07ac53
deidave <atudav15@gmail.com>
2025-04-18 21:22:03 +0300

    Feat: Filters banner on products page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import Benefits from "../components/Benefits";
 import Hero from "../components/Hero";
+import IconButton from "../components/IconButton";
+import { HiViewGrid } from "react-icons/hi";
+import { BsViewList } from "react-icons/bs";
+import { RiEqualizerLine } from "react-icons/ri";
 
 const Shop = () => {
+  const [show, setShow] = React.useState(16);
+  const [sort, setSort] = React.useState("default");
   return (
     <div className="shop">
       <main>
         <Hero title={"shop"} />
-        <div className="display-options"></div>
+        <div className="display-options">
+          <div className="one">
+            <div>
+              <IconButton icon={<RiEqualizerLine size={20} />} />
+              <h5>Filter</h5>
+            </div>
+            <IconButton icon={<HiViewGrid size={20} />} />
+            <IconButton icon={<BsViewList size={20} />} />
+            <p>Showing 1-16 of 32 items</p>
+          </div>
+          <form>
+            <div>
+              <h4>Show</h4>
+              <input
+                type="number"
+                name="show"
+                value={show}
+                onChange={(e) => setShow(e.target.value)}
+              />
+            </div>
+            <div>
+              <h4>Sort by</h4>
+              <input
+                type="text"
+                name="sort"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
         <div className="products"></div>
       </main>
       <Benefits />
