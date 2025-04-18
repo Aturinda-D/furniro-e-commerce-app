0cf363fc792ecc4a9c5e4552f18c2cd84d155a16
deidave <atudav15@gmail.com>
2025-04-18 14:29:53 +0300

    Fix: Made benefits banner into component

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import SummaryListItem from "../components/SummaryListItem";
+import { GrTrophy } from "react-icons/gr";
+import { BsPatchCheck } from "react-icons/bs";
+
+const Benefits = () => {
+  return (
+    <div className="benefits">
+      <SummaryListItem
+        icon={"assets/logos/trophy.svg"}
+        title={"high quality"}
+        subtitle={"Crafted from top materials"}
+      />
+      <SummaryListItem
+        icon={"assets/logos/check-badge.svg"}
+        title={"warranty protection"}
+        subtitle={"For over 2 years"}
+      />
+      <SummaryListItem
+        icon={"assets/logos/shipping.svg"}
+        title={"free shipping"}
+        subtitle={"Order over 150 $"}
+      />
+      <SummaryListItem
+        icon={"assets/logos/customer-support.svg"}
+        title={"24/7 support"}
+        subtitle={"Dedicated support"}
+      />
+    </div>
+  );
+};
+
+export default Benefits;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import SummaryListItem from "../components/SummaryListItem";
-import { GrTrophy } from "react-icons/gr";
-import { BsPatchCheck } from "react-icons/bs";
+import Benefits from "../components/Benefits";
 
 const Shop = () => {
   return (
@@ -10,28 +8,7 @@ const Shop = () => {
         <h1>SHOP</h1>
         <h5>Coming soon</h5>
       </main>
-      <div className="benefits">
-        <SummaryListItem
-          icon={"assets/logos/trophy.svg"}
-          title={"high quality"}
-          subtitle={"Crafted from top materials"}
-        />
-        <SummaryListItem
-          icon={"assets/logos/check-badge.svg"}
-          title={"warranty protection"}
-          subtitle={"For over 2 years"}
-        />
-        <SummaryListItem
-          icon={"assets/logos/shipping.svg"}
-          title={"free shipping"}
-          subtitle={"Order over 150 $"}
-        />
-        <SummaryListItem
-          icon={"assets/logos/customer-support.svg"}
-          title={"24/7 support"}
-          subtitle={"Dedicated support"}
-        />
-      </div>
+      <Benefits />
     </div>
   );
 };
