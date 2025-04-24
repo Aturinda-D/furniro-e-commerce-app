1b446cb01c9c16c6e2fa461391a4755d2126c1c3
deidave <atudav15@gmail.com>
2025-04-24 16:24:08 +0300

    Responsive navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,17 @@ import { MdOutlineAccountCircle } from "react-icons/md";
 import { Link } from "react-router-dom";
 import IconButton from "./IconButton";
 import { IoIosSearch, IoMdHeartEmpty } from "react-icons/io";
-import { IoCartOutline } from "react-icons/io5";
+import { IoCartOutline, IoClose } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 
-const Header = () => {
+const Header = ({ sideBarIsVisible, setSideBarIsVisible }) => {
   return (
     <header>
       <div className="logo">
         <img src="assets/logos/logo.png" alt="LOGO" />
         <h1>Furniro</h1>
       </div>
-      <nav>
+      <nav className="hidden-on-mobile">
         <ul>
           <li>
             <Link to={"/"}>Home</Link>
@@ -33,7 +34,41 @@ const Header = () => {
         <IconButton icon={<IoIosSearch size={24} />} />
         <IconButton icon={<IoMdHeartEmpty size={24} />} />
         <IconButton icon={<IoCartOutline size={24} />} />
+        <IconButton
+          icon={<RxHamburgerMenu size={24} />}
+          className="menu icon-button"
+          onClick={() => {
+            setSideBarIsVisible(true);
+          }}
+        />
       </div>
+      {sideBarIsVisible && (
+        <nav className="side-bar">
+          <ul>
+            <li>
+              <IconButton
+                icon={<IoClose size={24} />}
+                className="close icon-button"
+                onClick={() => {
+                  setSideBarIsVisible(false);
+                }}
+              />
+            </li>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/shop"}>Shop</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const [sideBarIsVisible, setSideBarIsVisible] = useState(false);
+  useEffect(() => {
+    setSideBarIsVisible(false);
+  }, [location]);
   return (
     <div className="root-layout">
-      <Header />
+      <Header
+        sideBarIsVisible={sideBarIsVisible}
+        setSideBarIsVisible={setSideBarIsVisible}
+      />
       <Outlet />
       <Footer />
     </div>
